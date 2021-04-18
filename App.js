737b9102313911d533ba656e3f5c6deaf65c226a
Cactiw <Knyazev1514@gmail.com>
2021-04-18 15:18:21 +0300

    Added bottomNavigationBar

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto"/>
+        </View>
+    )
+}
+
+
+function DetailScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>This is the details screen</Text>
+            <StatusBar style="auto"/>
+        </View>
+    )
+}
+
+
+const Tab = createMaterialBottomTabNavigator();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Home">
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Details" component={DetailScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
