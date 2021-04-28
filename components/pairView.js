1cd59bad3238e5f00ce5c28ae13ce96ec72e0bb1
Cactiw <Knyazev1514@gmail.com>
2021-04-28 14:11:13 +0300

    Updated PairView styles

--- components/pairView.js
+++ components/pairView.js
@@ -1,29 +1,36 @@
-import {StyleSheet, Text, View} from "react-native";
+import {ImageBackground, StyleSheet, Text, View} from "react-native";
 import React from "react";
-import Colors from "react-native/Libraries/NewAppScreen/components/Colors";
+import {Colors} from "react-native-paper";
+import {Divider} from "react-native-elements";
 
 
-export function pairView({ route, navigation }) {
+export function pairView({route, navigation}) {
     const {pairItem, pairDate} = route.params
     return (
         <View>
             <View style={styles.pairHeader}>
-                <Text style={styles.pairTime}>{pairItem.begin_clear_time} — {pairItem.end_clear_time}</Text>
-                <Text>Занятие</Text>
-                <Text>{pairItem.subject}</Text>
+                <ImageBackground source={require("../assets/copybook_1.jpg")} style={styles.image}>
+                    <View style={styles.pairHeaderContainer}>
+                        <Text style={[styles.pairTime, styles.pairHeaderMargin]}>{pairItem.begin_clear_time} — {pairItem.end_clear_time}</Text>
+                        <Text style={[styles.pairHeaderType, styles.pairHeaderMargin]}>Занятие</Text>
+                        <Text style={[styles.pairHeaderSubject, styles.pairHeaderMargin]}>{pairItem.subject}</Text>
+                    </View>
+                </ImageBackground>
             </View>
             <View>
-                <View>
-                    <Text>Аудитория</Text>
-                    <Text>{pairItem.auditorium.name}</Text>
+                <View style={styles.pairDetailsItem}>
+                    <Text style={styles.pairDetailsHeader}>Аудитория</Text>
+                    <Text style={styles.pairDetailsText}>{pairItem.auditorium.name}</Text>
                 </View>
-                <View>
-                    <Text>Преподаватель</Text>
-                    <Text>{pairItem.teacher.fullname}</Text>
+                <Divider/>
+                <View style={styles.pairDetailsItem}>
+                    <Text style={styles.pairDetailsHeader}>Преподаватель</Text>
+                    <Text style={styles.pairDetailsText}>{pairItem.teacher.fullname}</Text>
                 </View>
-                <View>
-                    <Text>Группа</Text>
-                    {/*<Text>{pairItem.group.name}</Text>*/}
+                <Divider/>
+                <View style={styles.pairDetailsItem}>
+                    <Text style={styles.pairDetailsHeader}>Группа</Text>
+                    <Text style={styles.pairDetailsText}>{pairItem.group.name}</Text>
                 </View>
             </View>
         </View>
@@ -32,11 +39,47 @@ export function pairView({ route, navigation }) {
 
 const styles = StyleSheet.create({
     pairHeader: {
-        backgroundColor: "#140024",
+        height: "50%"
+    },
+    pairHeaderContainer: {
+        marginLeft: "3%",
+        padding: 10,
+    },
+    pairHeaderMargin: {
+        margin: 10
+    },
+    pairHeaderSubject: {
+        color: Colors.white,
+        fontWeight: "bold",
+        fontSize: 18,
+        flexWrap: 'wrap',
+        width: "65%",
+    },
+    pairHeaderType: {
+        color: Colors.lightGreen100,
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
     },
     pairTime: {
-        fontSize: 17,
+        fontSize: 28,
         fontWeight: "bold",
         color: Colors.white,
-    }
+    },
+
+    pairDetailsItem: {
+        margin: 10,
+    },
+    pairDetailsHeader: {
+        margin: 5,
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+    pairDetailsText: {
+        margin: 5,
+    },
 })
